6d1411111aeba5127245979cd7d994afec08d425
ghazi200 <techworldstarzllc@gmail.com>
2025-02-24 20:53:54 -0500

    Fix Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all JS/TS files in the src directory
+    "./src/**/*.{html,js}",       // Include all HTML/JS files in the src directory
+    "./src/**/*.css",              // Include all CSS files in the src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
